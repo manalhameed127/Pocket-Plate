05e8c0ff19c52d8db417a08da6f345dc210d6012
Manal Hameed <manalhameed50@gmail.com>
2026-05-03 00:30:59 +0500

    INFRA-03: Socket.IO setup for real-time group ordering

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const connectDB = require("./db");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.json());
 
@@ -22,9 +31,28 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
+// Socket.IO
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_group", (groupId) => {
+    socket.join(groupId);
+    console.log(`User joined group: ${groupId}`);
+  });
+
+  socket.on("update_order", (data) => {
+    io.to(data.groupId).emit("order_updated", data);
+    console.log("Order updated in group:", data.groupId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 const errorHandler = require("./middleware/errorHandler");
 app.use(errorHandler);
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("Server running on port 5000");
 });
